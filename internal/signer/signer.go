cef7367e3e5ce388f6f4589296b35930edfaf908
Shingi <sechanakira@gmail.com>
2023-12-20 13:08:25 +0200

    feat: refactor out common validations

--- internal/signer/signer.go
+++ internal/signer/signer.go
@@ -5,85 +5,118 @@ import (
 	"database/sql"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
 	conn "test-signer/internal/db"
 	"time"
 )
 
 func SignAnswers(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "method not allowed here", http.StatusMethodNotAllowed)
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Println("failed to close response body")
+		}
+	}(r.Body)
+
+	if err := validateHttpMethod(r.Method, "POST"); err != nil {
+		writeError(w, http.StatusMethodNotAllowed, "Method not allowed here")
 		return
 	}
 
 	var req SignRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		writeError(w, http.StatusBadRequest, "Invalid request format")
 		return
 	}
 
-	if req.UserID == "" {
-		http.Error(w, "user id cannot be null", http.StatusBadRequest)
-		return
-	}
-
-	if req.Answers == nil {
-		http.Error(w, "answers are required", http.StatusBadRequest)
+	if err := validateRequest(req); err != nil {
+		writeError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	signature := generateSignature(req)
-
-	err := saveSession(req.UserID, signature, req.Answers)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if err := saveSession(req.UserID, signature, req.Answers); err != nil {
+		writeError(w, http.StatusInternalServerError, "Internal server error")
 		return
 	}
 
-	resp := SignResponse{Signature: signature}
+	writeJSONResponse(w, http.StatusCreated, SignResponse{Signature: signature})
+}
 
+func validateHttpMethod(method string, allowedMethod string) error {
+	if method != allowedMethod {
+		return fmt.Errorf("method not allowed")
+	}
+	return nil
+}
+
+func validateRequest(req SignRequest) error {
+	if req.UserID == "" {
+		return fmt.Errorf("user id cannot be null")
+	}
+	if len(req.Answers) == 0 {
+		return fmt.Errorf("answers are required")
+	}
+	return nil
+}
+
+func writeError(w http.ResponseWriter, statusCode int, message string) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(statusCode)
+	err := json.NewEncoder(w).Encode(map[string]string{"error": message})
+	if err != nil {
+		log.Printf("failed to write error %s ", err)
+		return
+	}
+}
 
-	json.NewEncoder(w).Encode(resp)
+func writeJSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		log.Printf("failed to write json response %s", err)
+		return
+	}
 }
 
 func VerifySignature(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "method not allowed here", http.StatusMethodNotAllowed)
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Printf("failed to close response body %s ", err)
+		}
+	}(r.Body)
+
+	if err := validateHttpMethod(r.Method, "POST"); err != nil {
+		writeError(w, http.StatusMethodNotAllowed, "Method not allowed here")
 		return
 	}
-
-	db, _ := conn.InitDB()
 
 	var req struct {
 		UserID    string `json:"userId"`
 		Signature string `json:"signature"`
 	}
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		writeError(w, http.StatusBadRequest, "Invalid request format")
 		return
 	}
 
-	if req.UserID == "" {
-		http.Error(w, "userId required", http.StatusBadRequest)
+	if err := validateVerifyRequest(req); err != nil {
+		writeError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	if req.Signature == "" {
-		http.Error(w, "signature required", http.StatusBadRequest)
-		return
-	}
-
-	var answers json.RawMessage
-	var createdAt time.Time
-	err := db.QueryRow("SELECT answers, created_at FROM user_session WHERE user_id = $1 AND signature = $2", req.UserID, req.Signature).Scan(&answers, &createdAt)
+	answers, createdAt, err := fetchSessionData(req.UserID, req.Signature)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			http.Error(w, "No matching session found", http.StatusNotFound)
+		if errors.Is(err, sql.ErrNoRows) {
+			writeError(w, http.StatusNotFound, "No matching session found")
 		} else {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			writeError(w, http.StatusInternalServerError, "Internal server error")
 		}
 		return
 	}
@@ -98,9 +131,38 @@ func VerifySignature(w http.ResponseWriter, r *http.Request) {
 		Timestamp: createdAt,
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	writeJSONResponse(w, http.StatusOK, response)
+}
 
-	json.NewEncoder(w).Encode(response)
+func validateVerifyRequest(req struct {
+	UserID    string `json:"userId"`
+	Signature string `json:"signature"`
+}) error {
+	if req.UserID == "" {
+		return fmt.Errorf("userId required")
+	}
+	if req.Signature == "" {
+		return fmt.Errorf("signature required")
+	}
+	return nil
+}
+
+func fetchSessionData(userID, signature string) (json.RawMessage, time.Time, error) {
+	db, dbErr := conn.InitDB()
+	if dbErr != nil {
+		return nil, time.Time{}, dbErr
+	}
+
+	var answers json.RawMessage
+	var createdAt time.Time
+
+	err := db.QueryRow("SELECT answers, created_at FROM user_session WHERE user_id = $1 AND signature = $2",
+		userID, signature).Scan(&answers, &createdAt)
+	if err != nil {
+		return nil, time.Time{}, err
+	}
+
+	return answers, createdAt, nil
 }
 
 func generateSignature(req SignRequest) string {
