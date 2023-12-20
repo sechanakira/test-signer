97c7890dfe2c1e8b4ded2a1ba360fd19ddbc0761
Shingi <sechanakira@gmail.com>
2023-12-20 12:40:44 +0200

    feat: fetch credentials from config

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,21 +2,33 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
+
 	_ "github.com/lib/pq"
 )
 
 func InitDB() (*sql.DB, error) {
-	dsn := "host=localhost port=5432 user=postgres password=changeit dbname=wonderland sslmode=disable"
-
+	dsn := getDSN()
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to open database: %w", err)
 	}
 
-	err = db.Ping()
-	if err != nil {
-		return nil, err
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
 
 	return db, nil
+}
+
+func getDSN() string {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSL_MODE")
+
+	return "host=" + host + " port=" + port + " user=" + user + " password=" + password + " dbname=" + dbname + " sslmode=" + sslmode
 }
